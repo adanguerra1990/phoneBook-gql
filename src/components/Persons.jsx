7003dc949809f2ae51f0c2e5d63bdc08001d0734
adanguerra1990 <aj90guerra@gmail.com>
2025-02-11 14:38:49 -0400

    Agregar componente Persons para buscar y mostrar detalles de una persona

---
+++ src/components/Persons.jsx
@@ -0,0 +1,55 @@
+import { gql, useLazyQuery } from '@apollo/client'
+import { useEffect, useState } from 'react'
+
+const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+const Persons = ({ persons }) => {
+  const [getPerson, result] = useLazyQuery(FIND_PERSON)
+  const [person, setPerson] = useState(null)
+
+  const showPerson = name => {
+    getPerson({ variables: { nameToSearch: name } })
+  }
+
+  useEffect(() => {
+    if (result.data) {
+      setPerson(result.data.findPerson)
+    }
+  }, [result])
+
+  if (person) {
+    return (
+      <div>
+        <h2>{person.name}</h2>
+        <div>phone: {person.phone}</div>
+        <div>street: {person.address.street}</div>
+        <div>city: {person.address.city}</div>
+        <button onClick={() => setPerson(null)}>Close</button>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h2>Persons</h2>
+      {persons.map(p => (
+        <div key={p.name}>
+          {p.name} {p.phone}
+          <button onClick={() => showPerson(p.name)}>show address</button>
+        </div>
+      ))}
+    </div>
+  )
+}
+export default Persons
