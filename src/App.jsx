aa24157c08162d5551e808ece15ff16ae7f47219
adanguerra1990 <aj90guerra@gmail.com>
2025-02-11 14:38:31 -0400

    Agregar componente App con consulta de Apollo para obtener y mostrar personas

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { gql, useQuery } from '@apollo/client'
+import Persons from './components/Persons'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      id
+      name
+      phone
+    }
+  }
+`
+
+function App() {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <>
+      <Persons persons={result.data.allPersons} />
+    </>
+  )
+}
+
+export default App
