0bbf9645a4035a8c54df9493921c11a0937c255f
adanguerra1990 <aj90guerra@gmail.com>
2025-02-11 15:13:10 -0400

    Agregar consultas y mutaciones de Apollo para gestionar personas

---
+++ src/queries.js
@@ -0,0 +1,43 @@
+import { gql } from '@apollo/client'
+
+export const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      id
+      name
+      phone
+    }
+  }
+`
+
+export const FIND_PERSON = gql`
+  query findPersonByName($nameToSearch: String!) {
+    findPerson(name: $nameToSearch) {
+      name
+      phone
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+export const CREATE_PESRON = gql`
+  mutation createPerson(
+    $name: String!
+    $phone: String!
+    $street: String!
+    $city: String!
+  ) {
+    addPerson(name: $name, phone: $phone, street: $street, city: $city) {
+      name
+      phone
+      id
+      address {
+        street
+        city
+      }
+    }
+  }
+`
