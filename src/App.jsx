2fce63d1a3394e5b75ac03fd89eee2c172fcfeab
adanguerra1990 <aj90guerra@gmail.com>
2025-02-12 14:40:50 -0400

    Agregar componente PhoneForm para cambiar números de teléfono y mutación correspondiente

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
 import { useState } from 'react'
 import Notify from './components/Notify'
+import PhoneForm from './components/PhoneForm'
 
 function App() {
   const [errorMessage, setErrorMessage] = useState(null)
@@ -26,6 +27,7 @@ function App() {
       <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
     </>
   )
 }
