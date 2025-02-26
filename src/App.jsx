f08950a265dbe681ddd6c7ebe1b9ea86b07ff13c
adanguerra1990 <aj90guerra@gmail.com>
2025-02-26 16:42:37 -0400

    Agregar funcionalidad de inicio de sesión y cierre de sesión en el componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
-import { useQuery } from '@apollo/client'
+import { useApolloClient, useQuery } from '@apollo/client'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import { ALL_PERSONS } from './queries'
 import { useState } from 'react'
 import Notify from './components/Notify'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 function App() {
   const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
 
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>Loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const notify = message => {
@@ -22,9 +31,20 @@ function App() {
     }, 5000)
   }
 
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <h2>Login</h2>
+        <LoginForm setToken={setToken} setError={notify} />
+      </div>
+    )
+  }
+
   return (
     <>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
