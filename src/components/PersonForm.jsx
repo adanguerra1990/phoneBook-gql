e43256ff87b638844ab2cde44a35de257f51e9a1
adanguerra1990 <aj90guerra@gmail.com>
2025-02-27 10:46:16 -0400

    Agregar funcionalidad para actualizar la caché al añadir una nueva persona en el formulario

--- src/components/PersonForm.jsx
+++ src/components/PersonForm.jsx
@@ -14,12 +14,26 @@ const PersonForm = ({ setError }) => {
       const messages = error.graphQLErrors.map(e => e.message).join('\n')
       setError(messages)
     },
+    update: (cache, response) => {
+      cache.updateQuery({ query: ALL_PERSONS }, ({ allPersons }) => {
+        return {
+          allPersons: allPersons.concat(response.data.addPerson),
+        }
+      })
+    },
   })
 
   const submit = event => {
     event.preventDefault()
 
-    createPerson({ variables: { name, phone, street, city } })
+    createPerson({
+      variables: {
+        name,
+        street,
+        city,
+        phone: phone.length > 0 ? phone : undefined,
+      },
+    })
 
     setName('')
     setPhone('')
